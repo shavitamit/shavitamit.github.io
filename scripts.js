e91e40cfb324206f0e149743079d84d02d89f45c
Amit Shavit <shavitamit@gmail.com>
2015-11-08 18:26:04 -0500

    Add Angular JS compatibility

---
+++ scripts.js
@@ -0,0 +1,126 @@
+var app = angular.module('app',['ngRoute','ngSanitize']) ;
+
+app.config(function($routeProvider) {
+
+$routeProvider
+	
+	// route for home page
+	.when('/', {
+		templateUrl: 'pages/home.html',
+		controller: 'HomeController'
+	})
+
+
+	// route for other pages
+	.when('/resume', {
+		templateUrl: 'pages/resume.html',
+		//controller: 'ResumeController',
+		activetab: 'resume/cv'
+	})
+
+	.when('/publications', {
+		templateUrl: 'pages/publications.html',
+		//controller: 'PublicationsController',
+		activetab: 'publications'
+	})
+
+
+	// route for other pages
+	.when('/contact', {
+		templateUrl: 'pages/contact.html',
+		//controller: 'ContactController',
+		activetab: 'contact'
+	});
+
+});
+
+app.controller('MainController', function($scope,$route) {
+	
+	$scope.message = 'Hello wrold mayne!' ;
+	$scope.$route = $route ; 
+
+
+});
+
+app.controller('NavController', ['$scope', '$route', function($scope, $route) {
+	
+	$scope.pages = {
+			'home' : '#/',
+			'resume/cv' : '#/resume',
+			'publications' : '#/publications',
+			'contact' : '#/contact'
+	};
+
+	$scope.isActive = function(page) {
+
+		// Fix the page variable for resume/cv
+		//if ( page == 'resume/cv' ) page = 'resume' ;
+
+		return (page === $route.current.activetab) ? true : false
+	};
+
+
+}]);
+
+
+/*
+app.controller('NavController', ['$scope', '$sce', '$route', '$location', function($scope, $sce, $route, $location) {
+
+	$scope.pages = {
+		'home' : '#/',
+		'resume/cv' : '#/resume',
+		'publications' : '#/publications',
+		'contact' : '#/contact'
+	};
+
+
+	$scope.getClass = function(page) {
+		// if ($route.current.activetab == page)
+		// 	return 'active' ;
+
+		console.log(('/' + page) === $location.path());
+
+		if (('/' + page) === $location.path())
+			// return 'active';
+			return true;
+		else
+			return false;
+	};
+
+	$scope.listPages = function() {
+		ret = '' ;
+
+		for (key in pages) {
+			// ret += '<li><a href="' + pages[key] + '" ng-class="' + $scope.getClass(key) + '">' + key + '</a></li>' ;
+			ret += '<li><a href="' + pages[key] + '" ng-class="{active:' + $scope.getClass(key) + '}">' + key + '</a></li>' ;
+			// ret += '<li><a href="' + pages[key] + '" ng-class="{active: $route.current.activetab == ' + key + '">' + key + '</a></li>' ;
+		}
+
+		return ret
+	};
+
+	$scope.pages = $sce.trustAsHtml($scope.listPages()) ;
+	
+
+}]); */
+
+
+
+
+app.controller('HomeController', function($scope) {
+	
+	$scope.message = 'Home Page' ;
+	
+});
+
+app.controller('AboutController', function($scope) {
+	
+	$scope.message = 'About me' ;
+
+});
+
+app.controller('ContactController', function($scope) {
+	
+	$scope.message = 'Contact Me' ;
+	
+});
